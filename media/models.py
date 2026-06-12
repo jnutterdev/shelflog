d91365c130c0b58ccf9d95954e1a7a9a52358ca9
jnutterdev <hello@jnutterdev.com>
2026-06-12 12:41:30 -0400

    Setting up a media shelf log app in django

---
+++ media/models.py
@@ -0,0 +1,24 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+
+# Create your models here.
+class MediaItem(models.Model):
+    title = models.CharField(max_length=255)
+    release_year = models.IntegerField(
+        validators=[MinValueValidator(1000), MaxValueValidator(9999)]
+    )
+    rating = models.IntegerField(
+        blank=True,
+        null=True,
+    )
+    notes = models.TextField(
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return str(self.title)
