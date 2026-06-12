b666de7774a2c6c3fe706f0ad4bcf02dbf53fa13
jnutterdev <hello@jnutterdev.com>
2026-06-12 17:51:18 -0400

    Adding entries

--- media/admin.py
+++ media/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from media.models import Album, Book, Film
+
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Film)
+admin.site.register(Album)
