f7887de43e20096c35d81e8383c72ee2438b2214
jnutterdev <hello@jnutterdev.com>
2026-06-12 22:59:41 -0400

    Updated model, added serch filters

--- media/admin.py
+++ media/admin.py
@@ -2,7 +2,47 @@ from django.contrib import admin
 
 from media.models import Album, Book, Film
 
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "author",
+        "release_year",
+    ]
+    search_fields = [
+        "title",
+        "author",
+        "release_year",
+    ]
+
+
+class FilmAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "director",
+        "release_year",
+    ]
+    search_fileds = [
+        "title",
+        "director",
+        "release_year",
+    ]
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "artist",
+        "release_year",
+    ]
+    search_fields = [
+        "title",
+        "artist",
+        "release_year",
+    ]
+
+
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Film)
-admin.site.register(Album)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Film, FilmAdmin)
+admin.site.register(Album, AlbumAdmin)
